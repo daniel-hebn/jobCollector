65ed7a91e4adc4e45305cd38097793457792591c
daniel82 <daniel82@coupang.com>
2016-08-03 01:36:59 +0900

    mysql 설정 추가 - 추후 profile 별로 나누기

---
+++ src/test/java/com/hebn/jobCollector/StringTest.java
@@ -0,0 +1,12 @@
+package com.hebn.jobCollector;
+
+/**
+ * Created by coupang on 2016. 8. 2..
+ */
+public class StringTest {
+
+    public static void main(String[] args) {
+        int preIndex = "http://stackoverflow.com/jobs/111547/ios-developer-icemobile".indexOf("http://stackoverflow.com/jobs/");
+        System.out.println(preIndex);
+    }
+}
