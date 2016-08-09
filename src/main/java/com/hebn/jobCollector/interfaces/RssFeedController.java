74e4ffd8be6355e5318257c529db08ee9b87169f
daniel82 <daniel82@coupang.com>
2016-08-10 01:28:35 +0900

    단순 hello Controller 추가

--- src/main/java/com/hebn/jobCollector/interfaces/RssFeedController.java
+++ src/main/java/com/hebn/jobCollector/interfaces/RssFeedController.java
@@ -1,7 +1,17 @@
 package com.hebn.jobCollector.interfaces;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by greg.lee on 2016. 8. 1..
  */
+@RestController
 public class RssFeedController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return "hello - job Collector";
+    }
 }
