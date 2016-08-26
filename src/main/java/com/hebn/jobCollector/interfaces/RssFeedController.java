87e67deff801b90b3a8d5cabd4b20527df9fbc17
daniel82 <daniel82@coupang.com>
2016-08-27 00:39:31 +0900

    map test

--- src/main/java/com/hebn/jobCollector/interfaces/RssFeedController.java
+++ src/main/java/com/hebn/jobCollector/interfaces/RssFeedController.java
@@ -1,17 +1,25 @@
 package com.hebn.jobCollector.interfaces;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by greg.lee on 2016. 8. 1..
  */
-@RestController
+@Controller
 public class RssFeedController {
 
+    @ResponseBody
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
         return "hello - job Collector";
+    }
+
+    @RequestMapping(value = "/naver-map", method = RequestMethod.GET)
+    public String naverMapTest() {
+        return "naver-test";
     }
 }
